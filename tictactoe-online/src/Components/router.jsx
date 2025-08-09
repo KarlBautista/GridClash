72c981babcc8b3b0a6e096b9187f5ce622dba5a3
Karl <karlbautista234@gmail.com>
2025-08-09 18:32:31 +0800

    partial implementation of the online mode

--- tictactoe-online/src/Components/router.jsx
+++ tictactoe-online/src/Components/router.jsx
@@ -4,13 +4,15 @@ import Menu from "./Menu"
 import React from 'react'
 import App from "../App"
 import TicTacToeComputer from "./TicTacToeComputer"
+import PlayerVsPlayerOnline from "./PlayerVsPlayerOnline"
 const router = createBrowserRouter([{
     path: "/",
     element: <App />,
     children: [
         { path: "/", element: <Menu />},
         { path: "/player-vs-player-local", element: <Tictactoe />},
-        { path: "/player-vs-computer", element: <TicTacToeComputer />}
+        { path: "/player-vs-computer", element: <TicTacToeComputer />},
+        { path: "/player-vs-player-online", element: <PlayerVsPlayerOnline />}
     ]
 }])
 
